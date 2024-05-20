4e74d9f4dad099fa1fe56a70c6d56c3d7dd50fbf
batskywalker <60446365+batskywalker@users.noreply.github.com>
2024-05-19 23:18:01 -0700

    it shows as an activity

--- client/main.js
+++ client/main.js
@@ -1,5 +1,49 @@
+import {DiscordSDK} from "@discord/embedded-app-sdk";
+
 import './style.css'
 import rocketLogo from '/rocket.png'
+
+let auth;
+const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
+
+setupDiscordSdk().then(() => {
+  console.log("Discord SDK is ready");
+});
+
+async function setupDiscordSdk() {
+  await discordSdk.ready();
+  console.log("Discord SDK is ready");
+
+  const {code} = await discordSdk.commands.authorize({
+    client_id: import.meta.env.VITE_DISCORD_CLIENT_ID,
+    response_type: "code",
+    state: "",
+    prompt: "none",
+    scope: [
+      "identify",
+      "guilds",
+    ],
+  });
+
+  const response = await fetch("/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      code,
+    }),
+  });
+  const {access_token} = await response.json();
+
+  auth = await discordSdk.commands.authenticate({
+    access_token,
+  });
+
+  if (auth == null) {
+    throw new Error("Authenticate command failed");
+  }
+}
 
 document.querySelector('#app').innerHTML = `
   <div>
